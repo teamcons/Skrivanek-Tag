5494e081645eb66e6f3a05313c7c2b16f41d2e59
teamcons_atwork <littlepastelworld@gmail.com>
2024-04-11 13:56:27 +0200

    f

--- main.ps1
+++ main.ps1
@@ -366,7 +366,7 @@ elseif ($file.FullName -match "^M:\\9_JOBS_XTRF\\20[0-9][0-9]_[0-9][0-9][0-9][0-
 
     # Grab studio
     # Sometimes the files are in a subfolder and theres several trados folders. So Search the correct one
-    $STUDIO	= (Get-ChildItem *trados,*studio).FullName
+    $STUDIO	= (Get-ChildItem *trados,*studio | Select-Object -First 1).FullName
     $STUDIO	= (Get-ChildItem  -Path $STUDIO -Filter (-join($file.BaseName,".sdlproj")) -Recurse).Directory.FullName
 
     # If the script cant find for shit, just grab basefolder
